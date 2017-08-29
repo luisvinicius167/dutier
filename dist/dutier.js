e70684ba13e3590345328e45f0a439e16e2001b6
Luis Vinicius <luis@startamus.com.br>
2017-08-29 19:38:28 -0300

    immutable state

--- dist/dutier.js
+++ dist/dutier.js
@@ -33,19 +33,13 @@ var Provider = {
  */
 var create = (function (state) {
   return function (state) {
-    var state = Object.assign({}, state);
     return function (current) {
       state = Object.assign({}, state, current);
-      return state;
+      return Object.assign({}, state);
     };
   }(state);
 });
 
-/**
- * @name setReducer
- * @description Set the reducer function, the
- * initial state of the reducer and store state
- */
 var setReducer = (function (reducers) {
   // if createStore don't was called yet
   if (Provider._updateState({}) === undefined) {
@@ -60,12 +54,6 @@ var setReducer = (function (reducers) {
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-/**
- * Async Reducer
- * Just dispatch if return new state values.
- * With this, the subscribe function will not be
- * called unnecessary, because the state don't be changed
- */
 var asyncReducer = (function (action) {
   return new Promise(function (resolve) {
     var _iteratorNormalCompletion = true;
@@ -106,9 +94,6 @@ var asyncReducer = (function (action) {
   });
 });
 
-/**
- * Apply the subscribe handler functions
- */
 var applyHandler = (function (_ref) {
   var type = _ref.type,
       state = _ref.state;
@@ -128,31 +113,16 @@ var applyMiddleware = (function (data) {
   return Promise.resolve({ type: data.action.type, state: data.state });
 });
 
-/**
-   * @name dispatch
-   * @description Dispatch an action to change
-   * the store state
-   * @param { Object } payload The action payload
-   */
 var dispatch = (function (payload) {
   return new Promise(function (resolve) {
     return payload.call(null, resolve);
   }).then(asyncReducer).then(applyMiddleware).then(applyHandler);
 });
 
-/**
- * @name getState
- * @return {Object} a copy of the state
- */
 var getState = (function () {
   return Provider._updateState({});
 });
 
-/**
- * @name unsubscribe
- * @description Unsubscribes from listening to a component
- * @param {Function} handler The handler function
- **/
 var unsubscribe = (function (handler) {
   Provider._handlers.forEach(function (fn, index) {
     if (fn === handler) {
@@ -161,12 +131,6 @@ var unsubscribe = (function (handler) {
   });
 });
 
-/**
- * Subscribe to receive notifications when state is updated.
- * @name subscribe
- * @description Subscribe to call the handler function when the action will be triggered
- * @param {Function} handler The function that will be called
- **/
 var subscribe = (function (handler) {
   Provider._handlers.push(handler);
   return function () {
@@ -174,12 +138,6 @@ var subscribe = (function (handler) {
   };
 });
 
-/**
- * @name createStore
- * @description Sets the store state
- * @param {Object} data Simple Object that contain the State
- * @param {Function} reducers The action reducers
- */
 var createStore = (function () {
   for (var _len = arguments.length, reducers = Array(_len), _key = 0; _key < _len; _key++) {
     reducers[_key] = arguments[_key];
@@ -192,10 +150,6 @@ var createStore = (function () {
   return { dispatch: dispatch, subscribe: subscribe, getState: getState };
 });
 
-/**
- * @name combine
- * @description Combine the reducers
- */
 var combine = (function () {
   for (var _len = arguments.length, reducers = Array(_len), _key = 0; _key < _len; _key++) {
     reducers[_key] = arguments[_key];
