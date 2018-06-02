2a3d03276759aa2718e20040ae327c12343d0350
Luis Vinicius <luisvinicius167@Mac-mini-de-Luis.local>
2018-06-02 17:43:35 -0300

    payloads over values

--- src/asyncReducer.js
+++ src/asyncReducer.js
@@ -2,7 +2,7 @@ import { Provider } from './providers'
 
 /**
  * Async Reducer
- * Just dispatch if return new state values.
+ * Just dispatch if return new state payloads.
  * With this, the subscribe function will not be
  * called unnecessary, because the state don't be changed
  */
@@ -11,7 +11,7 @@ export default action => {
     Provider._reducers.forEach(({ reducer, initial }) => {
       const stateReducer = reducer.current ? reducer.current : initial
       const current = reducer.current = reducer(stateReducer, action, Provider._updateState({}))
-      const reducerOldState = reducer(stateReducer, { type: '@@Dutier.OLD_STATE', value: action.value })
+      const reducerOldState = reducer(stateReducer, { type: '@@Dutier.OLD_STATE', payload: action.payload })
       const oldState = Object.assign({}, Provider._updateState({}), reducerOldState)
       if (JSON.stringify( current ) !== JSON.stringify(stateReducer)) {
         return resolve({ action, oldState, state: Provider._updateState(current) })
