cff2b311c31e13242e41d1a4da961e6441c99292
Luis Vinicius <luisvinicius167@Mac-mini-de-Luis.local>
2018-06-02 17:43:24 -0300

    pass payload to middleware

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -2,5 +2,5 @@ import { Provider } from './providers'
 
 export default data => {
   Provider._middlewares.forEach( middleware => middleware.call(null, data) )
-  return Promise.resolve({ type:data.action.type, state: data.state });
+  return Promise.resolve({ ...data.action, state: data.state });
 }
